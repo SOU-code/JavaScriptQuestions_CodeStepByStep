314a33e25b93eb5fee756a32973d25b03f88f658
tech-souvik <officialsouvikmaji@gmail.com>
2023-09-12 08:27:35 +0530

    2 question

--- JsTrickyInterviewQuestions/script.js
+++ JsTrickyInterviewQuestions/script.js
@@ -1,10 +1,27 @@
 // Question -1
-(function () {
-  var a = b = 3
-})();
-console.log(typeof a);
-console.log(typeof b);
+// (function () {
+//   var a = b = 3
+// })();
+// console.log(typeof a);
+// console.log(typeof b);
 /*Output:
 undefined
 number
 (But If we use strict mode answer is reference error: b is not defined)*/
+
+//Question -2
+function foo() {
+    return {
+        name:'sanju'
+    }
+}
+console.log(foo());
+/* Output: {name:'sanju'} */
+function foo2() {
+    return
+    {
+        name:'sanju'
+    }
+}
+console.log(foo2());
+/* Output: undefined (return jeta hbe seta sob somoi return er line theke jano start hoi noito js compiler vabe return kichu kora hoini)  */
